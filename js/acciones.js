de7e5fa1310aa3f88818a06bc4685909fa7e4e29
yarfernandez <alanferuizreach@gmail.com>
2019-05-21 14:16:12 -0500

    4

--- js/acciones.js
+++ js/acciones.js
@@ -1,18 +1,18 @@
 //JavaScript Document
 
 $(document).ready(function(e){
-	document.addEventListener("deviceready", onDeviceReady,false);
+	document.addEventListener("deviceready",onDeviceReady,false);
 		
 });
 
-		function onDeviceReady() {
+		function onDeviceReady(){
 			
 			$('#posicion').on('click', function(){
 				getPosition ();
 			});
 		}
 		
-		function getPosition() {
+		function getPosition(){
 			var options ={
 				enableHighAccuracy :true,
 				maximumAge: 3600000
@@ -20,14 +20,14 @@ $(document).ready(function(e){
 			
 			var watchID = navigator.geolocation.getCurrentPosition(onSuccess,onError,options);
 			
-			function onSuccess(position) {
+			function onSuccess(position){
 				
 				alert('Latitude:'   + position.coords.latitude    +'\n'+
 				'Longitude:'       + position.coords.longitude     +'\n');
 			};
 			
-			function onError(error) {
-				alert('code:'  + error.code  + '\n'+'message:' + error.message + '\n');
+			function onError(error){
+				alert('code:'  + error.code  +'\n'+'message:' + error.message +'\n');
 			}
 		}
 				
